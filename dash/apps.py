14efcdb29cc4a7aa85e95f9ec42a5eecedfcad5f
Ryan Hockett <ryanhockett.dev@gmail.com>
2025-06-23 10:03:38 -0500

    Added home url route, view, and template

--- dash/apps.py
+++ dash/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class PlaygroundConfig(AppConfig):
+class DashConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'playground'
+    name = 'dash'
--- pythonApp/settings.py
+++ pythonApp/settings.py
@@ -36,8 +36,8 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'playground',
-    'debug_toolbar'
+    'debug_toolbar',
+    'dash'
 ]
 
 MIDDLEWARE = [
